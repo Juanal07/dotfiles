1447552d70a156dbfd4bf10eff5aa2a0320ce3b5
Juan Alberto Raya <22559891+Juanal07@users.noreply.github.com>
2022-01-05 16:59:06 +0100

    auto push

--- nvim/.config/nvim/lua/user/lsp/handlers.lua
+++ nvim/.config/nvim/lua/user/lsp/handlers.lua
@@ -84,10 +84,11 @@ local function lsp_keymaps(bufnr)
 	-- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
 	vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()' ]])
 end
-
+-- para que no me pregunte con que lsp quiero formatear (siempre usare null-ls hasta que lsp traiga formateadores
 M.on_attach = function(client, bufnr)
-	if client.name == "tsserver" then
+	if client.name == "tsserver" or client.name == "jsonls" then
 		client.resolved_capabilities.document_formatting = false
+		client.resolved_capabilities.document_range_formatting = false
 	end
 	lsp_keymaps(bufnr)
 	lsp_highlight_document(client)
--- nvim/.config/nvim/lua/user/packer.lua
+++ nvim/.config/nvim/lua/user/packer.lua
@@ -101,7 +101,6 @@ return packer.startup(function(use)
 	use("neovim/nvim-lspconfig")
 	use("williamboman/nvim-lsp-installer")
 	use("jose-elias-alvarez/null-ls.nvim")
-	-- use("mhartington/formatter.nvim")
 	-- Completion
 	use("hrsh7th/nvim-cmp")
 	use("hrsh7th/cmp-buffer")
