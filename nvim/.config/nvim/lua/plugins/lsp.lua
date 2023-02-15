02ed66d226963875fbf80b556efc93703c25f0d4
Juan Alberto Raya <22559891+Juanal07@users.noreply.github.com>
2023-02-15 11:49:06 +0100

    Wip

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -5,89 +5,89 @@ require("mason-lspconfig").setup({})
 
 -- Diagnostic Signs
 local signs = {
-	{ name = "DiagnosticSignError", text = "" },
-	{ name = "DiagnosticSignWarn", text = "" },
-	{ name = "DiagnosticSignHint", text = "" },
-	{ name = "DiagnosticSignInfo", text = "" },
+    { name = "DiagnosticSignError", text = "" },
+    { name = "DiagnosticSignWarn",  text = "" },
+    { name = "DiagnosticSignHint",  text = "" },
+    { name = "DiagnosticSignInfo",  text = "" },
 }
 for _, sign in ipairs(signs) do
-	vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+  vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
 end
 
 -- Diagnostics
 local config = {
-	virtual_text = false,
-	severity_sort = true,
-	float = {
-		source = "always",
-		header = "",
-		-- prefix = "",
-	},
+    virtual_text = false,
+    severity_sort = true,
+    float = {
+        source = "always",
+        header = "",
+        -- prefix = "",
+    },
 }
 vim.diagnostic.config(config)
 
 -- LSP keymaps
 local function lsp_keymaps(bufnr)
-	local opts = { noremap = true, silent = false }
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(
-		bufnr,
-		"n",
-		"gr",
-		'<cmd>lua require"telescope.builtin".lsp_references({ includeDeclaration = false })<CR>',
-		opts
-	)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>fd", "<cmd>Telescope diagnostics<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+  local opts = { noremap = true, silent = false }
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(
+      bufnr,
+      "n",
+      "gr",
+      '<cmd>lua require"telescope.builtin".lsp_references({ includeDeclaration = false })<CR>',
+      opts
+  )
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>fd", "<cmd>Telescope diagnostics<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
 end
 
 -- On attach function
 local on_attach = function(client, bufnr)
-	lsp_keymaps(bufnr)
+  lsp_keymaps(bufnr)
 end
 
 local lspconfig = require("lspconfig")
 -- Servers
 require("mason-lspconfig").setup_handlers({
-	-- Lua
-	["sumneko_lua"] = function()
-		lspconfig.sumneko_lua.setup({
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { "vim" },
-					},
-				},
-			},
-			on_attach = on_attach,
-		})
-	end,
-	-- Python
-	["pyright"] = function()
-		lspconfig.pyright.setup({
-			settings = {
-				python = {
-					analysis = {
-						typeCheckingMode = "off",
-					},
-				},
-			},
-			on_attach = on_attach,
-		})
-	end,
-	-- Default
-	function(server_name)
-		lspconfig[server_name].setup({
-			on_attach = on_attach,
-		})
-	end,
+    -- Lua
+    ["lua_ls"] = function()
+      lspconfig.lua_ls.setup({
+          settings = {
+              Lua = {
+                  diagnostics = {
+                      globals = { "vim" },
+                  },
+              },
+          },
+          on_attach = on_attach,
+      })
+    end,
+    -- Python
+    ["pyright"] = function()
+      lspconfig.pyright.setup({
+          settings = {
+              python = {
+                  analysis = {
+                      typeCheckingMode = "off",
+                  },
+              },
+          },
+          on_attach = on_attach,
+      })
+    end,
+    -- Default
+    function(server_name)
+      lspconfig[server_name].setup({
+          on_attach = on_attach,
+      })
+    end,
 })
