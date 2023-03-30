6d03f1082d6f6b6abf82895e51291b122dd92a0d
Juan Alberto Raya <22559891+Juanal07@users.noreply.github.com>
2023-03-31 00:23:59 +0200

    Wip

--- nvim/.config/nvim/lua/packer-config.lua
+++ nvim/.config/nvim/lua/packer-config.lua
@@ -89,6 +89,7 @@ return packer.startup(function(use)
   use("nvim-telescope/telescope-media-files.nvim")
 
   -- use("gpanders/editorconfig.nvim")
+
   -- use({
   --   "iamcco/markdown-preview.nvim",
   --   run = function()
