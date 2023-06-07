f49faa7ee222c447f7806a55f703e49da8695587
Juan Alberto Raya <22559891+Juanal07@users.noreply.github.com>
2023-06-07 23:53:15 +0200

    Wip

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -14,10 +14,16 @@ require("plugins.gitsigns")
 
 require("neogit").setup()
 require("nvim-surround").setup()
-require("fidget").setup()
 require("nvim-autopairs").setup()
 require("nvim-web-devicons").setup()
 require("which-key").setup()
+require("fidget").setup(
+  {
+    window = {
+      blend = 0,
+    }
+  }
+)
 require("Comment").setup({
   pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
 })
