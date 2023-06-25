8e0c913b4df77a6f72e1ba7bf82eea038fb0875c
Juan Alberto Raya <22559891+Juanal07@users.noreply.github.com>
2023-06-25 19:34:34 +0200

    Wip

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -4,7 +4,7 @@ require("gitsigns").setup({
     -- add = { hl = "GitSignsAdd", text = "+", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
     -- change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
     -- delete = { hl = "GitSignsDelete", text = "a ", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    -- topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+    -- topdelete = { hl = "GitSignsDelete", text = "󰐊", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
     -- changedelete = {
     --   hl = "GitSignsChange",
     --   text = "a ",
