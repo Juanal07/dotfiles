a31c00f7dc6b1a20b280ea7c7fc30767d8f16dd2
Juan Alberto Raya <jjrr1307@gmail.com>
2023-12-09 23:27:46 +0100

    Wip

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -35,13 +35,13 @@ local highlight = {
 }
 local hooks = require("ibl.hooks")
 hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-	vim.api.nvim_set_hl(0, "GruvboxRed", { fg = "#FB4934" })
-	vim.api.nvim_set_hl(0, "GruvboxYellow", { fg = "#FABD2F" })
-	vim.api.nvim_set_hl(0, "GruvboxBlue", { fg = "#83A598" })
-	vim.api.nvim_set_hl(0, "GruvboxOrange", { fg = "#D65D0E" })
-	vim.api.nvim_set_hl(0, "GruvboxGreen", { fg = "#B8BB26" })
-	vim.api.nvim_set_hl(0, "GruvboxPurple", { fg = "#D3869B" })
-	vim.api.nvim_set_hl(0, "GruvboxAqua", { fg = "#8EC07C" })
+	vim.api.nvim_set_hl(0, "GruvboxRed", { fg = "#FB4934", underline = false }) -- Add underline = false to each line
+	vim.api.nvim_set_hl(0, "GruvboxYellow", { fg = "#FABD2F", underline = false })
+	vim.api.nvim_set_hl(0, "GruvboxBlue", { fg = "#83A598", underline = false })
+	vim.api.nvim_set_hl(0, "GruvboxOrange", { fg = "#D65D0E", underline = false })
+	vim.api.nvim_set_hl(0, "GruvboxGreen", { fg = "#B8BB26", underline = false })
+	vim.api.nvim_set_hl(0, "GruvboxPurple", { fg = "#D3869B", underline = false })
+	vim.api.nvim_set_hl(0, "GruvboxAqua", { fg = "#8EC07C", underline = false })
 end)
 vim.g.rainbow_delimiters = { highlight = highlight }
 require("ibl").setup({ scope = { highlight = highlight } })
