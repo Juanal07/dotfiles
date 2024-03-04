a3db57ce551ea41acd13120b424b027d0f01010b
Juan Alberto Raya <22559891+Juanal07@users.noreply.github.com>
2024-03-04 09:47:28 +0100

    Wip

--- nvim/.config/nvim/lua/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/plugins/null-ls.lua
@@ -2,6 +2,33 @@ local null_ls = require("null-ls")
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
+local misspell = (function()
+	local h = require("null-ls.helpers")
+	local methods = require("null-ls.methods")
+
+	local DIAGNOSTICS = methods.internal.DIAGNOSTICS
+
+	return h.make_builtin({
+		name = "misspell",
+		meta = {
+			url = "https://github.com/client9/misspell",
+			description = "Checks commonly misspelled English words in source files.",
+		},
+		method = DIAGNOSTICS,
+		filetypes = {},
+		generator_opts = {
+			command = "misspell",
+			to_stdin = true,
+			format = "line",
+			on_output = h.diagnostics.from_pattern(
+				[[:(%d+):(%d+): (.*)]],
+				{ "row", "col", "message" },
+				{ diagnostic = { severity = h.diagnostics.severities["information"] }, offsets = { col = 1 } }
+			),
+		},
+		factory = h.generator_factory,
+	})
+end)()
 
 local async_formatting = function(bufnr)
 	bufnr = bufnr or vim.api.nvim_get_current_buf()
@@ -49,7 +76,7 @@ null_ls.setup({
 	debug = false,
 	sources = {
 		-- Spellchecking
-		-- diagnostics.misspell,
+		misspell,
 
 		-- Editorconfig
 		-- diagnostics.editorconfig_checker,
