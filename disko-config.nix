f0858115184f46539be80a7ee843d83c9f802c70
Juan Alberto Raya <jjrr1307@gmail.com>
2024-08-31 00:32:39 +0200

    Wip

--- disko-config.nix
+++ disko-config.nix
@@ -1,54 +1,9 @@
-# {
-#   disko.devices = {
-#     disk = {
-#       main = {
-#         type = "disk";
-#         device = "/dev/nvme0n1";
-#         content = {
-#           type = "gpt";
-#           partitions = {
-#             ESP = {
-#               name = "ESP";
-#               start = "2MiB";
-#               end = "1GiB";
-#               type = "EF00";
-#               content = {
-#                 type = "filesystem";
-#                 format = "vfat";
-#                 mountpoint = "/boot";
-#               };
-#             };
-#             swap = {
-#               name = "swap";
-#               start = "1GiB";
-#               end = "9GiB";
-#               content = {
-#                 type = "swap";
-#                 randomEncryption = true;
-#               };
-#             };
-#             root = {
-#               name = "root";
-#               start = "9GiB";
-#               end = "100%";
-#               content = {
-#                 type = "filesystem";
-#                 format = "ext4";
-#                 mountpoint = "/";
-#               };
-#             };
-#           };
-#         };
-#       };
-#     };
-#   };
-# }
 {
   disko.devices = {
     disk = {
-      my-disk = {
-        device = "/dev/nvme0n1";
+      main = {
         type = "disk";
+        device = "/dev/nvme0n1";
         content = {
           type = "gpt";
           partitions = {
@@ -62,11 +17,18 @@
               };
             };
             root = {
-              size = "100%";
+              end = "-8G";
               content = {
                 type = "filesystem";
                 format = "ext4";
                 mountpoint = "/";
+              };
+            };
+            swap = {
+              size = "100%";
+              content = {
+                type = "swap";
+                randomEncryption = true;
               };
             };
           };
