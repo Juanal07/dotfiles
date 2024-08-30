d1d0370310448362c7459093c146420ebf917403
Juan Alberto Raya <jjrr1307@gmail.com>
2024-08-31 00:19:13 +0200

    Wip

--- disko-config.nix
+++ disko-config.nix
@@ -1,36 +1,68 @@
+# {
+#   disko.devices = {
+#     disk = {
+#       main = {
+#         type = "disk";
+#         device = "/dev/nvme0n1";
+#         content = {
+#           type = "gpt";
+#           partitions = {
+#             ESP = {
+#               name = "ESP";
+#               start = "2MiB";
+#               end = "1GiB";
+#               type = "EF00";
+#               content = {
+#                 type = "filesystem";
+#                 format = "vfat";
+#                 mountpoint = "/boot";
+#               };
+#             };
+#             swap = {
+#               name = "swap";
+#               start = "1GiB";
+#               end = "9GiB";
+#               content = {
+#                 type = "swap";
+#                 randomEncryption = true;
+#               };
+#             };
+#             root = {
+#               name = "root";
+#               start = "9GiB";
+#               end = "100%";
+#               content = {
+#                 type = "filesystem";
+#                 format = "ext4";
+#                 mountpoint = "/";
+#               };
+#             };
+#           };
+#         };
+#       };
+#     };
+#   };
+# }
 {
   disko.devices = {
     disk = {
-      main = {
+      my-disk = {
+        device = "/dev/sda";
         type = "disk";
-        device = "/dev/nvme0n1";
         content = {
           type = "gpt";
           partitions = {
             ESP = {
-              name = "ESP";
-              start = "2MiB";
-              end = "1GiB";
               type = "EF00";
+              size = "500M";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
               };
             };
-            # swap = {
-            #   name = "swap";
-            #   start = "1GiB";
-            #   end = "9GiB";
-            #   content = {
-            #     type = "swap";
-            #     randomEncryption = true;
-            #   };
-            # };
             root = {
-              name = "root";
-              start = "9GiB";
-              end = "100%";
+              size = "100%";
               content = {
                 type = "filesystem";
                 format = "ext4";
